fea766461f1a1df71f5015815ca4036de4841bef
Arends1 <135931064+Arends1@users.noreply.github.com>
2026-01-20 16:14:08 -0400

    Update DashboardDueño.jsx

--- src/componentes/dueño/DashboardDueño.jsx
+++ src/componentes/dueño/DashboardDueño.jsx
@@ -31,9 +31,9 @@ const DashboardDueño = () => {
       setCargando(true);
       
       const [inventarioRes, usuariosRes, configRes] = await Promise.all([
-        fetch('http://127.0.0.1:8000/inventario'),
-        fetch('http://127.0.0.1:8000/usuarios'),
-        fetch('http://127.0.0.1:8000/configuraciones')
+        fetch('https://constrefri-backend.onrender.com/inventario'),
+        fetch('https://constrefri-backend.onrender.com/usuarios'),
+        fetch('https://constrefri-backend.onrender.com/configuraciones')
       ]);
 
       if (inventarioRes.ok && usuariosRes.ok && configRes.ok) {
